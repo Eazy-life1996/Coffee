db72895c0906b56b27fac0e8969e4311f39647c8
Yurii <eazylifemusic@gmail.com>
2022-03-02 16:49:35 +0600

    Продолжаю работу над хэдером

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ btn.addEventListener("click", function () {
   menu.classList.toggle('active');
   btn.classList.toggle('active-btn');
 });
+let btnForInput = document.querySelector('.header__button');
+let inputSearch = document.querySelector('.header__input');
+
+btnForInput.addEventListener("click", function () {
+  inputSearch.classList.toggle('header__input--active');
+});
